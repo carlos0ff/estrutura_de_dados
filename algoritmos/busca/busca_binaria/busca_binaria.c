5c991e91124ce1ee365d53a5da0a1db3957397c5
carlso0ff <carlosiilva66@gmail.com>
2026-03-10 13:52:48 -0300

    Refatoração da busca binária: separação das implementações em C e Java

--- algoritmos/busca/busca_binaria/busca_binaria.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "busca_binaria.h"
-
-void busca_binaria_sort(int arr[], int n) {
-    // Implementação do algoritmo aqui
-    printf("Função busca_binaria_sort chamada.\n");
-}
---
+++ algoritmos/busca/busca_binaria/java/Main.java
@@ -0,0 +1,11 @@
+package algoritmos.busca.busca_binaria.java;
+
+public class Main {
+    /**
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("BUSCA BINARIA");
+    }
+}
